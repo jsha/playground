e6b0c73968d4c29c40e683b69aebfd51af6340a9
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2016-04-21 22:38:53 -0700

    Genericize metric.

--- metrics.py
+++ metrics.py
@@ -1,26 +1,29 @@
 #!/usr/bin/env python
+import plotly
+
+import ConfigParser
+import datetime
+import json
+import requests
+import sqlite3
 import datetime
 import urllib2
-import json
-import sqlite3
-import requests
 
-metric="PAGELOAD_IS_SSL"
 s = requests.Session()
 conn = sqlite3.connect("metrics.sqlite3")
 
 # Also do:
 # HTTP_TRANSACTION_IS_SSL
 # CERT_VALIDATION_SUCCESS_BY_CA
-def get_stat(version, date):
+def get_stat(version, date, metric):
     # First try to read from DB.
-    c.execute("SELECT count, sum FROM pageload_is_ssl WHERE version=? and date=?", (version, date))
+    c.execute("SELECT count, sum FROM metrics WHERE version=? and date=? and metric=?", (version, date, metric))
     for (count, total) in c.fetchall():
         return count, total
     # Otherwise fetch from origin.
     print "fetch", version, date
     r = requests.get('https://aggregates.telemetry.mozilla.org/aggregates_by/submission_date/channels/release/?version=%d&dates=%s&metric=%s' % (
-        version, date, metric))
+        version, date, metric.upper()))
     # API consistently returns 404 for certain dates / versions.
     if r.status_code == 404:
         return None, None
@@ -33,8 +36,8 @@ def get_stat(version, date):
         entry = data['data'][0]
         print entry
         count, total = entry['histogram'][1], sum(entry['histogram'])
-        c.execute("INSERT INTO pageload_is_ssl (date, version, count, sum) VALUES(?,?,?,?)",
-            (date, version, count, total))
+        c.execute("INSERT INTO metrics (date, version, metric, count, sum) VALUES(?,?,?,?,?)",
+            (date, version, metric, count, total))
         conn.commit()
         return count, total
     else:
@@ -42,44 +45,86 @@ def get_stat(version, date):
 
 c = conn.cursor()
 c.execute("""
-    CREATE TABLE IF NOT EXISTS pageload_is_ssl
+    CREATE TABLE IF NOT EXISTS metrics
     (date TEXT NOT NULL,
      version INTEGER NOT NULL,
+     metric TEXT NOT NULL,
      count INTEGER,
      sum INTEGER,
-     PRIMARY KEY (date, version))
+     PRIMARY KEY (date, version, metric))
 """)
 conn.commit()
 
-def fetch():
+def fetch(metric):
     current_date = datetime.date.today()
-    c.execute("select max(version) from pageload_is_ssl")
+    c.execute("select max(version) from metrics")
     max_version = c.fetchall()[0][0]
-    for ago in range(0, 365):
+    if max_version is None:
+        max_version = 44
+    for ago in range(2, 250):
         # Check all versions up to the highest one seen, plus one. This way we automatically
         # adapt to new versions.
         for version in range(39, max_version + 1):
             fetch_date = current_date + datetime.timedelta(-ago)
             formatted_date = fetch_date.strftime('%Y%m%d')
-            count, total = get_stat(version, formatted_date)
+            count, total = get_stat(version, formatted_date, metric)
     conn.commit()
 
-def plot():
+def plot(metric):
     c.execute("""
-        select date, sum(count) * 100.0 / sum(sum) as percent from pageload_is_ssl
-            where count is not null group by date;
-    """)
+        select date, sum(count) * 100.0 / sum(sum) as percent from metrics
+            where count is not null and metric=? group by date;
+    """, (metric,))
     percents = []
     dates = []
-    for (date, percent) in c.fetchall():
-        percents.append(percent)
-        if len(percents) < 28:
+    percents_graph = plotly.graph_objs.Scatter(
+        name='Daily',
+        mode='markers',
+        marker=dict(
+            color="rgb(130, 130, 130)",
+            size=3
+        ),
+        x=[], y=[])
+    average_graph = plotly.graph_objs.Scatter(
+        name='28-day centered moving average',
+        line=dict(
+            color="rgb(0, 0, 0)",
+        ),
+        x=[], y=[])
+    results = c.fetchall()
+    for i in range(0, len(results)):
+        date = datetime.datetime.strptime(results[i][0], "%Y%m%d")
+        percents_graph.x.append(date)
+        percents_graph.y.append(results[i][1])
+        if i <= 14 || i > len(results) - 14:
             continue
-        print date, percents[14], sum(percents) / 28
-        percents.pop(0)
+        average = sum([x[1] for x in results[i-14:i+13]]) / 28
+        average_graph.x.append(date)
+        average_graph.y.append(average)
 
-fetch()
-plot()
+    config = ConfigParser.ConfigParser()
+    config.read(".config")
+    plotly.session.sign_in(
+      config.get("plotly", "user"),
+      config.get("plotly", "apikey")
+    )
+
+    layout = plotly.graph_objs.Layout(
+      title="Percentage of pageloads over HTTPS",
+      yaxis=dict(rangemode='tozero')
+    )
+
+    try:
+      #figure = plotly.graph_objs.Figure(data=percents_graph_obj, layout=layout)
+      figure = plotly.graph_objs.Figure(data=[percents_graph, average_graph], layout=layout)
+      plot_url = plotly.offline.plot(figure, filename='metrics.html')
+      print(plot_url)
+    except plotly.exceptions.PlotlyError as pe:
+      print(pe)
+
+
+fetch("http_pageload_is_ssl")
+plot("http_pageload_is_ssl")
 
 conn.close()
 s.close()
